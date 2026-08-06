713a935aaf2e0be49b146fc3c38cddc4d3c0e832
YEERRin <ryeon0107@gmail.com>
2026-08-06 14:52:49 +0900

    [yerin] 1m^2당오브젝트 수에 따른 이미지 변화 추가

--- unitySystem/Assets/Scripts/PeopleDensityUI.cs
+++ unitySystem/Assets/Scripts/PeopleDensityUI.cs
@@ -4,11 +4,15 @@ using UnityEngine.UI;
 public class PeopleDensityUI : MonoBehaviour
 {
     public Text densityText; // "1 /��"
+    public Image densityImage;
 
     public void SetDensity(float density)
     {
         // Display density as a rounded integer value.
         densityText.text = $"{Mathf.RoundToInt(density)} people/m²";
+
+        //밀도에 따른 이미지 색상 변경
+        ChangeImageColor(density);
 
         // Pass density to DangerDetector.
         if (DangerDetector.Instance != null)
@@ -16,4 +20,25 @@ public class PeopleDensityUI : MonoBehaviour
             DangerDetector.Instance.UpdateDensity(density);
         }
     }
-}
+
+    //이미지 변경 
+    void ChangeImageColor(float density)
+    {
+        if (density <= 3)
+        {
+            densityImage.color = Color.green;
+        }
+
+        else if (density <=7) {
+
+            densityImage.color = Color.yellow;
+        }
+
+        else
+        {
+            densityImage.color = Color.red;
+        }
+    }
+  }
+
+
