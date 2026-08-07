abf799b590cfdd0293d7463f522b3f5a6d62742e
Kimhaelyn <81563342+Kimhaelyn@users.noreply.github.com>
2026-08-07 10:30:12 +0900

    Update comments for clarity in people_data.py

--- dataGenerator/crud/people_data.py
+++ dataGenerator/crud/people_data.py
@@ -2,13 +2,13 @@ from pymongo.collection import Collection
 from bson import ObjectId
 from typing import Optional
 
-#새 데이터 db에 추가 
+# Insert the new data into the database
 def create_document(collection: Collection, doc: dict):
     
     result = collection.insert_one(doc)
     return str(result.inserted_id)
 
-# 이전 데이터 가져오기 
+# Retrieve the previous data
 def get_latest_document(collection: Collection):
     return collection.find_one(sort=[("datetime", -1)])
 
