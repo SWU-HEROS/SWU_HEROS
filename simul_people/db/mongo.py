a6a76d73e7443b2ced8dd6c9e8d301d9075ade1c
Kimhaelyn <81563342+Kimhaelyn@users.noreply.github.com>
2026-08-07 10:42:04 +0900

    Update mongo.py

--- simul_people/db/mongo.py
+++ simul_people/db/mongo.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
 
 def connect_mongo(collection_name):
-    #docker에서
-    #client = MongoClient("mongodb://admin:admin@mongodb:27017/HEROS?authSource=admin")
+    # For Docker
+    # client = MongoClient("mongodb://admin:admin@mongodb:27017/HEROS?authSource=admin")
     client = MongoClient("mongodb://localhost:27017")
     db = client['HEROS']
     collection = db[collection_name]
