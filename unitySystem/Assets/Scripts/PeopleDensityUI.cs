951ee62a3347b9b6f5e3726f025201e5d3000bfa
Kimhaelyn <81563342+Kimhaelyn@users.noreply.github.com>
2026-08-07 11:04:37 +0900

    Update comments and formatting in PeopleDensityUI
    Refactor comments for clarity and consistency.

--- unitySystem/Assets/Scripts/PeopleDensityUI.cs
+++ unitySystem/Assets/Scripts/PeopleDensityUI.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
 public class PeopleDensityUI : MonoBehaviour
 {
-    public Text densityText; // "1 /��"
+    public Text densityText;
     public Image densityImage;
 
     public void SetDensity(float density)
@@ -11,7 +11,7 @@ public class PeopleDensityUI : MonoBehaviour
         // Display density as a rounded integer value.
         densityText.text = $"{Mathf.RoundToInt(density)} people/m²";
 
-        //밀도에 따른 이미지 색상 변경
+        // Change the image color based on the density
         ChangeImageColor(density);
 
         // Pass density to DangerDetector.
@@ -21,7 +21,7 @@ public class PeopleDensityUI : MonoBehaviour
         }
     }
 
-    //이미지 변경 
+    // Change the image color
     void ChangeImageColor(float density)
     {
         if (density <= 3)
@@ -29,8 +29,8 @@ public class PeopleDensityUI : MonoBehaviour
             densityImage.color = Color.green;
         }
 
-        else if (density <=7) {
-
+        else if (density <= 7)
+        {
             densityImage.color = Color.yellow;
         }
 
@@ -39,6 +39,4 @@ public class PeopleDensityUI : MonoBehaviour
             densityImage.color = Color.red;
         }
     }
-  }
-
-
+}
